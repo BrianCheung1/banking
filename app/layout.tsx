648ed4221974596d95185bec4a0557781b492688
Brian Cheung <44447718+BrianCheung1@users.noreply.github.com>
2024-09-11 12:29:37 -0400

    add force-dynamic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,33 @@
-import type { Metadata } from "next";
-import { Inter, IBM_Plex_Serif } from "next/font/google";
-import "./globals.css";
+export const dynamic = "force-dynamic"
 
-const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+import type { Metadata } from "next"
+import { Inter, IBM_Plex_Serif } from "next/font/google"
+import "./globals.css"
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" })
 const ibmPlexSerif = IBM_Plex_Serif({
-  subsets:['latin'], 
-  weight: ['400', '700'], 
-  variable: '--font-ibm-plex-serif'
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-ibm-plex-serif",
 })
 export const metadata: Metadata = {
   title: "Banking App",
   description: "Modern Banking App for Everyone",
   icons: {
-    icon:'/icons/logo.svg'
-  }
-};
+    icon: "/icons/logo.svg",
+  },
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}</body>
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>
+        {children}
+      </body>
     </html>
-  );
+  )
 }
