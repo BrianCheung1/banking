425f6939457b5524051940e682592e97dc050bde
Brian Cheung <44447718+BrianCheung1@users.noreply.github.com>
2024-09-11 12:15:15 -0400

    implement transfer and transactions tables

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -6,7 +6,11 @@ import {
   usePlaidLink,
 } from "react-plaid-link"
 import { useRouter } from "next/navigation"
-import { createLinkToken, exchangePublicToken } from "@/lib/actions/user.actions"
+import {
+  createLinkToken,
+  exchangePublicToken,
+} from "@/lib/actions/user.actions"
+import Image from "next/image"
 
 const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
   const [token, setToken] = useState("")
@@ -14,8 +18,8 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
 
   useEffect(() => {
     const getLinkToken = async () => {
-      const data = await createLinkToken(user);
-        setToken(data?.linkToken)
+      const data = await createLinkToken(user)
+      setToken(data?.linkToken)
     }
 
     getLinkToken()
@@ -23,10 +27,10 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
 
   const onSuccess = useCallback<PlaidLinkOnSuccess>(
     async (public_token: string) => {
-        await exchangePublicToken({
-          publicToken: public_token,
-          user,
-        })
+      await exchangePublicToken({
+        publicToken: public_token,
+        user,
+      })
 
       router.push("/")
     },
@@ -50,9 +54,28 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
           Connect Bank
         </Button>
       ) : variant === "ghost" ? (
-        <Button>Connect Bank</Button>
+        <Button
+          className="plaidlink-ghost"
+          onClick={() => open()}
+          variant="ghost"
+        >
+          <p className="hidden text-[16px] font-semibold text-black-2 xl:block">
+            Connect Bank
+          </p>
+        </Button>
       ) : (
-        <Button>Connect Bank</Button>
+        <Button className="plaidlink-default" onClick={() => open()}>
+          <div className="relative size-6">
+            <Image
+              src="/icons/connect-bank.svg"
+              alt="connect bank"
+              fill
+            />
+          </div>
+          <p className="text-[16px] font-semibold text-black-2 max-xl:hidden">
+            Connect Bank
+          </p>
+        </Button>
       )}
     </>
   )
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,6 +7,7 @@ import Link from "next/link"
 import { usePathname } from "next/navigation"
 import React from "react"
 import Footer from "./Footer"
+import PlaidLink from "./PlaidLink"
 
 const Sidebar = ({ user }: SiderbarProps) => {
   const pathname = usePathname()
@@ -39,7 +40,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
                   alt={item.label}
                   fill
                   className={cn({ "brightness-[3] invert-0": isActive })}
-                ></Image>
+                />
               </div>
               <p className={cn('sidebar-label', {'!text-white':isActive})}>
                 {item.label}
@@ -48,7 +49,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
           )
         })}
 
-        USER
+        <PlaidLink user={user}/>
       </nav>
       <Footer user={user}/>
     </section>
